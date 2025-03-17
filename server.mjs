3f3aea7ed45f56d71f1f62125e7d6d157de882bb
phoneayemin <phoneayemin@better.hr>
2025-03-17 16:25:43 +0700

    server room creating

--- server.mjs
+++ server.mjs
@@ -1,6 +1,7 @@
 import { Server } from "socket.io";
 import http from "http";
 import express from "express";
+
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
@@ -11,17 +12,27 @@ const io = new Server(server, {
 
 io.on("connection", (socket) => {
   console.log("User connected: ", socket.id);
-
-  socket.on("message", (msg) => {
-    console.log("message", msg);
-    io.emit("message", msg);
+  // Join a dynamic room
+  socket.on("join-room", (room) => {
+    socket.join(room);
+    console.log(`User ${socket.id} joined room: ${room}`);
+    socket.emit("joined-room", `Joined room: ${room}`);
   });
 
-  socket.on("send-message", (msg) => {
-    console.log("message", msg);
-    io.emit("message", msg);
+  // Send message to a specific room
+  socket.on("send-message", ({ room, message }) => {
+    console.log(`Message to ${room}:`, message);
+    io.to(room).emit("message", { sender: socket.id, message });
   });
 
+  // Leave a room
+  socket.on("leave-room", (room) => {
+    socket.leave(room);
+    console.log(`User ${socket.id} left room: ${room}`);
+    socket.emit("left-room", `Left room: ${room}`);
+  });
+
+  // Handle disconnection
   socket.on("disconnect", () => {
     console.log("User disconnected:", socket.id);
   });
